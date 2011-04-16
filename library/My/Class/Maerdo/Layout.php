d85b30e3042a2c9506d15f7bdcf9fceb921622c1
Nicolas Blaudez <nblaudez@maerdo.com>
2011-04-16 14:47:28 +0200

    Debugging

---
+++ library/My/Class/Maerdo/Layout.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This class is used to work with LAyout.
+ * 
+ * @author Nicolas Blaudez <nblaudez@maerdo.com>
+ * @package Maerdo
+ * @version 0.1
+ */
+
+class My_Class_Maerdo_Layout {
+		
+	public static function renderingMaerdoSidebar(Zend_Controller_Request_Abstract $request,$view) {
+		$sidebar_file=null;
+		$controller=strtolower($request->getControllerName());
+		$action=strtolower($request->getActionName());
+				
+		$scriptspaths=$view->getScriptPaths();
+		foreach($scriptspaths as $path) {
+			if(is_file($path.'snippets/sidebar/'.$controller.'/'.$action.'.phtml')) {
+				$sidebar_file='snippets/sidebar/'.$controller.'/'.$action.'.phtml';
+			}
+		}	
+		return($sidebar_file);										
+	}
+}
